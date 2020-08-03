9be1b727617ff14f41c9237399945be33f73683d
Lakshman Bondela <lbondela@sprucetech.com>
2020-08-03 00:16:47 -0400

    Added filter feature

--- app.js
+++ app.js
@@ -11,41 +11,44 @@ loadEventListeners();
 function loadEventListeners() {
   //Add task event
   form.addEventListener("submit", addTask);
+  //Delete a Task
   taskList.addEventListener("click", deleteTaskEvent);
+  //Delete all tasks
+  clearBtn.addEventListener("click", clearTasks);
+  //Filter Tasks
+  filter.addEventListener("keyup", filterTasks);
 }
 
 function addTask(e) {
   e.preventDefault();
   if (taskInput.value === "") {
     alert("Add a Task");
+  } else {
+    //Create list item
+    const li = document.createElement("li");
+    li.className = "collection-item";
+
+    //Create Text Node and append to li
+    //li.appendChild(document.createTextNode(taskInput.value));
+    li.innerHTML = taskInput.value;
+
+    //create  new link item
+    const link = document.createElement("a");
+    link.className = "delete-item secondary-content";
+    //Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+
+    taskList.appendChild(li);
+    taskInput.value = "";
   }
-  //Create list item
-  const li = document.createElement("li");
-  li.className = "collection-item";
-
-  //Create Text Node and append to li
-  //li.appendChild(document.createTextNode(taskInput.value));
-  li.innerHTML = taskInput.value;
-
-  //create  new link item
-  const link = document.createElement("a");
-  link.className = "delete-item secondary-content";
-  //Add icon html
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  li.appendChild(link);
-
-  taskList.appendChild(li);
-  taskInput.value = "";
 }
-
-//Delete all tasks
-clearBtn.addEventListener("click", clearTasks);
 
 function clearTasks() {
   while (taskList.firstChild) {
     taskList.firstChild.remove();
+    //taskList.removeChild();
   }
-  //taskList.removeChild();
 }
 
 function deleteTaskEvent(e) {
@@ -59,4 +62,20 @@ function deleteTaskEvent(e) {
   /* if ((target.parentNode.tagName = "a")) {
     target.parentNode.parentNode.remove();
   } */
+}
+
+function filterTasks(e) {
+  const filterText = e.target.value.toLowerCase();
+  document.querySelectorAll(".collection-item").forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(filterText) != -1) {
+      task.classList.remove("hide");
+    } else {
+      task.classList.add("hide");
+    }
+    /* if (filterText === "") {
+      console.log("Here");
+      task.classList.add("displaying");
+    } */
+  });
 }
