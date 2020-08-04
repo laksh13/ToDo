f5831153447425c70e625cfa19e4e37ece8d3002
Lakshman Singh Bondela <laksh.singh@gmail.com>
2020-08-03 22:59:12 -0400

    Feature/persistance (#2)
    * Feature to add and remove tasks
    
    * Added filter feature
    
    * Add/delete and Load from local storage
    
    Co-authored-by: Lakshman Bondela <lbondela@sprucetech.com>

--- app.js
+++ app.js
@@ -9,43 +9,52 @@ const deleteTask = document.querySelector(".delete-item");
 loadEventListeners();
 
 function loadEventListeners() {
+  //Load existing Tasks from Local Storage
+  document.addEventListener("DOMContentLoaded", loadTasksfromStorage);
   //Add task event
   form.addEventListener("submit", addTask);
+  //Delete a Task
   taskList.addEventListener("click", deleteTaskEvent);
+  //Delete all tasks
+  clearBtn.addEventListener("click", clearTasks);
+  //Filter Tasks
+  filter.addEventListener("keyup", filterTasks);
 }
 
 function addTask(e) {
   e.preventDefault();
   if (taskInput.value === "") {
     alert("Add a Task");
+  } else {
+    //Create list item
+    const li = document.createElement("li");
+    li.className = "collection-item";
+
+    //Create Text Node and append to li
+    //li.appendChild(document.createTextNode(taskInput.value));
+    li.innerHTML = taskInput.value;
+
+    //create  new link item
+    const link = document.createElement("a");
+    link.className = "delete-item secondary-content";
+    //Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+
+    taskList.appendChild(li);
+    //Store the task in Local Storage
+    addTaskToLocalStorage(taskInput.value);
+    taskInput.value = "";
   }
-  //Create list item
-  const li = document.createElement("li");
-  li.className = "collection-item";
-
-  //Create Text Node and append to li
-  //li.appendChild(document.createTextNode(taskInput.value));
-  li.innerHTML = taskInput.value;
-
-  //create  new link item
-  const link = document.createElement("a");
-  link.className = "delete-item secondary-content";
-  //Add icon html
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  li.appendChild(link);
-
-  taskList.appendChild(li);
-  taskInput.value = "";
 }
-
-//Delete all tasks
-clearBtn.addEventListener("click", clearTasks);
 
 function clearTasks() {
   while (taskList.firstChild) {
     taskList.firstChild.remove();
+    //taskList.removeChild();
+    //Remove all tasks from Local Storage
+    localStorage.removeItem("taskList");
   }
-  //taskList.removeChild();
 }
 
 function deleteTaskEvent(e) {
@@ -53,10 +62,69 @@ function deleteTaskEvent(e) {
   if (target.parentNode.classList.contains("delete-item")) {
     target.parentNode.parentNode.remove();
   }
+  deleteTaskFromStorage(target.parentNode.parentNode.textContent);
   /* if (target.parentNode.className.indexOf("delete-item") != -1) {
     target.parentNode.parentNode.remove();
   } */
   /* if ((target.parentNode.tagName = "a")) {
     target.parentNode.parentNode.remove();
   } */
+}
+
+function filterTasks(e) {
+  const filterText = e.target.value.toLowerCase();
+  document.querySelectorAll(".collection-item").forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(filterText) != -1) {
+      task.classList.remove("hide");
+    } else {
+      task.classList.add("hide");
+    }
+    /* if (filterText === "") {
+      console.log("Here");
+      task.classList.add("displaying");
+    } */
+  });
+}
+
+function addTaskToLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem("taskList") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("taskList"));
+  }
+
+  tasks.push(task);
+  localStorage.setItem("taskList", JSON.stringify(tasks));
+}
+
+function loadTasksfromStorage() {
+  if (localStorage.getItem("taskList") !== null) {
+    let tasks = JSON.parse(localStorage.getItem("taskList"));
+    tasks.map((task) => {
+      const li = document.createElement("li");
+      li.className = "collection-item";
+
+      li.innerHTML = task;
+
+      const link = document.createElement("a");
+      link.className = "delete-item secondary-content";
+      link.innerHTML = '<i class="fa fa-remove"></i>';
+      li.appendChild(link);
+
+      taskList.appendChild(li);
+    });
+  }
+}
+
+function deleteTaskFromStorage(task) {
+  if (localStorage.getItem("taskList") !== null) {
+    let tasks = JSON.parse(localStorage.getItem("taskList"));
+    let pos = tasks.indexOf(task);
+    if (pos != -1) {
+      tasks.splice(pos, 1);
+    }
+    localStorage.setItem("taskList", JSON.stringify(tasks));
+  }
 }
